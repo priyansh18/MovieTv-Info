7f4aa2accd32c6d82faac3f168ce2136c1275481
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-12 18:11:03 +0530

    Basic Component Added

--- movie_search/src/App.js
+++ movie_search/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Text from './Components/Text'
 function App() {
   return (
     <div className="App">
-      Hello World!!!
+      <Text/>
     </div>
   );
 }
---
+++ movie_search/src/Components/Text.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Text extends Component {
+  render() { 
+    return ( <p>Hello from text1</p> );
+  }
+}
+ 
+export default Text;
