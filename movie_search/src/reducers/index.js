8235a707fd9f095291c80af6b389bc22180d5cf9
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-13 17:28:28 +0530

    Creating Search Actions

--- movie_search/src/reducers/index.js
+++ movie_search/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from "redux";
 import searchReducer from "./searchReducer";
 
 export default combineReducers({
-  momvies: searchReducer,
+  movies: searchReducer,
 });
--- movie_search/src/reducers/searchReducer.js
+++ movie_search/src/reducers/searchReducer.js
@@ -1,8 +1,17 @@
 import { SEARCH_MOVIE } from './../actions/types';
 
-const initialStates={
+const initialState={
   text='',
   movies:[],
   loading:false,
   movie:[]
+}
+
+export default function(state=initialState,action){
+  switch(action.type){
+    case SEARCH_MOVIE:
+      return {...state,text:action.payload,loading:false}
+    default:
+      return state  
+  }
 }
