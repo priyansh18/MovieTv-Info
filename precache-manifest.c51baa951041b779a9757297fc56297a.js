0c55fc125cdc7a5611b0914f6f6e09efb8c5823e
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-15 13:10:17 +0530

    Updates

---
+++ precache-manifest.c51baa951041b779a9757297fc56297a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a527193e42ca779253452dc3d566a4",
+    "url": "/MovieTv-info/index.html"
+  },
+  {
+    "revision": "ef0f44f4ff6f905a9774",
+    "url": "/MovieTv-info/static/css/main.cf7d33b7.chunk.css"
+  },
+  {
+    "revision": "2bd6db583ff32998bcd4",
+    "url": "/MovieTv-info/static/js/2.ae60cb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieTv-info/static/js/2.ae60cb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0f44f4ff6f905a9774",
+    "url": "/MovieTv-info/static/js/main.0283bc96.chunk.js"
+  },
+  {
+    "revision": "82290897db4ed764cbf6",
+    "url": "/MovieTv-info/static/js/runtime-main.b53e99d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/MovieTv-info/precache-manifest.83a553f9ef7f923a907b9ce9ce67a82a.js"
+  "/MovieTv-info/precache-manifest.c51baa951041b779a9757297fc56297a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/MovieTv-info/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MovieTv-info/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
