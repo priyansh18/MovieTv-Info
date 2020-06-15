d0c7d919fb6eccdc7cc89af11e2ac7d6100fbc56
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-15 13:07:12 +0530

    Updates

---
+++ precache-manifest.83a553f9ef7f923a907b9ce9ce67a82a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347a6121847fad2b4179689eba347a73",
+    "url": "/github.io/MovieTv-info/index.html"
+  },
+  {
+    "revision": "43ddcae1c375b05d7a67",
+    "url": "/github.io/MovieTv-info/static/css/main.cf7d33b7.chunk.css"
+  },
+  {
+    "revision": "2bd6db583ff32998bcd4",
+    "url": "/github.io/MovieTv-info/static/js/2.ae60cb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/MovieTv-info/static/js/2.ae60cb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ddcae1c375b05d7a67",
+    "url": "/github.io/MovieTv-info/static/js/main.87307cf1.chunk.js"
+  },
+  {
+    "revision": "ac2db54b04cd7d07be51",
+    "url": "/github.io/MovieTv-info/static/js/runtime-main.0777f9c6.js"
+  }
+]);
